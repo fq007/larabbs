2dd2d68a7b14d16ceba1c043066fe88900780005
fq007 <1291530566@qq.com>
2021-09-14 13:43:13 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://blog.csdn.net/qq_40812169/article/details/119580957?spm=1001.2014.3001.5501" target="_blank">Fengqian</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
